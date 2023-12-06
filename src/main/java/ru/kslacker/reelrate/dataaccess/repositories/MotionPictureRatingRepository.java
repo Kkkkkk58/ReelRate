70d678f99150c70b97f628724b6971d8991b69df
Karim Khasan <karimhasan3879@gmail.com>
2023-12-07 00:37:29 +0300

    feat: add UserDetails and UserDetailsService

--- src/main/java/ru/kslacker/reelrate/dataaccess/repositories/MotionPictureRatingRepository.java
+++ src/main/java/ru/kslacker/reelrate/dataaccess/repositories/MotionPictureRatingRepository.java
@@ -3,8 +3,10 @@ package ru.kslacker.reelrate.dataaccess.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.kslacker.reelrate.dataaccess.entities.MotionPictureRatingView;
 
+@Repository
 public interface MotionPictureRatingRepository extends JpaRepository<MotionPictureRatingView, Long> {
     Optional<MotionPictureRatingView> findByMotionPictureId(Long motionPictureId);
 }
